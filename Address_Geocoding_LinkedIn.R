932d098cf50e9c7d97262ac5470d14a53f17b110
AbdullahSallam <sallam.abdullah@gmail.com>
2024-08-24 18:56:03 +0300

    Add files via upload

---
+++ Address_Geocoding_LinkedIn.R
@@ -0,0 +1,19 @@
+library(tidygeocoder)
+library(tidyverse)
+#========================================================
+# reading source sheet to dataframe
+location_file <- read.csv("C:\\......csv") #write full path
+
+#select the column that contains address. Not needed if the source sheet has only one column
+location_list<-data.frame(location_file$Location)
+
+#create destination data frame for storing the geocoding output
+coordinates<-data.frame("address","latitude","Lonngitude")
+
+#Loop for geocoding every single location
+for (i in 1:nrow(addr_list)) {
+  coordinates_df[nrow(coordinates_df)+1,]<-geo_arcgis(location_list[i, ]) 
+}
+
+#writing the output dataframe to an csv file
+write.csv(coordinates_df,file = "C:\\Users\\DELL\\Downloads\\geo_refrence_output.csv")
